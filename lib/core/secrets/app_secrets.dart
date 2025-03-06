aad9f8d01b85bfc7f91d92083344ceb68ad085b7
AkhrievSuleym <murid2005@mail.ru>
2025-03-07 02:05:54 +0300

    some_changes

--- lib/core/secrets/app_secrets.dart
+++
@@ -1,5 +0,0 @@
-class AppSecrets {
-  static const supabaseUrl = "https://nhydgauvgtshnurmhxvp.supabase.co";
-  static const supabaseKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oeWRnYXV2Z3RzaG51cm1oeHZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEzMDA4NzEsImV4cCI6MjA1Njg3Njg3MX0.SkMq7WKXm4nniKaV1Q4o49YrxyL4LNYoRKSgkCLNP2c";
-}
